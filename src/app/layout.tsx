68bad3776d2f3492064cbeacf8ecb8aacbef4a38
Amartya Sinha <amartua2002@gmail.com>
2023-07-20 12:51:03 +0530

    removed themeprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import LenisProvider from '@/components/Providers/LenisProvider'
 import Header from '@/components/Header'
-import ThemeProvider from '@/components/Providers/ThemeProvider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,12 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white text-black antialiased dark:bg-black dark:text-white">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <Header />
-          <LenisProvider>
-            <main>{children}</main>
-          </LenisProvider>
-        </ThemeProvider>
+
+        {/* <Header /> */}
+        <LenisProvider>
+          <main>{children}</main>
+        </LenisProvider>
+
       </body>
     </html>
   )
