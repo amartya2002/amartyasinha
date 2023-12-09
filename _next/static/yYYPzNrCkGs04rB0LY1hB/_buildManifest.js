658e39035525408e1b04a2a92c0c72f9fc3ad629
Amartya Sinha <amartua2002@gmail.com>
2023-12-10 00:19:22 +0530

    Updates

---
+++ _next/static/yYYPzNrCkGs04rB0LY1hB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2485f808813bcf3b.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
